757cd749652b0a11d384095d7d711d5b639994db
mateus040 <mateusgabrielmoreno264@gmail.com>
2024-04-26 20:57:15 -0300

    feat: header responsivo

--- MRFerreira.Web/src/components/Header/index.tsx
+++ MRFerreira.Web/src/components/Header/index.tsx
@@ -1,43 +1,62 @@
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
+import { useRef, useState } from "react";
 
 export default function Header() {
+  const [menuState, setMenuState] = useState<string>("menu");
+
+  const onToggleMenu = () => {
+    setMenuState((prevState) => (prevState === "menu" ? "close" : "menu"));
+  };
+
   return (
-    <div className="fixed flex top-0 left-0 right-0 justify-between bg-black px-10 lg:px-28 py-5">
-      <div>
-        <Link to="/">
-          {/*<img src="/logo-branco.png" className="h-20"/>*/}
-          <p className="text-white text-xl font-bold">MR Ferreira</p>
-          <p
-            className="text-white font-semibold mx-5"
-            style={{ fontSize: "12px" }}
+    <div>
+      <div className="bg-black">
+        <div className="flex justify-between items-center px-8 lg:px-20 py-5 mx-auto">
+          <div>
+            <p className="text-white text-xl font-bold">MR Ferreira</p>
+            <p className="text-white font-semibold mx-5" style={{ fontSize: "12px" }}>Representações</p>
+          </div>
+          <div
+            className={`nav-links duration-500 lg:static absolute bg-black lg:min-h-fit min-h-[60vh] left-0 ${
+              menuState === "menu" ? "top-[-100%]" : "top-[12.5%]"
+            } lg:w-auto w-full flex items-center px-5`}
           >
-            Representações
-          </p>
-        </Link>
-      </div>
+            <ul className="flex lg:flex-row flex-col lg:items-center gap-8">
+              <li className="mx-4 lg:mx-0">
+                <Link to="/" className="hover:text-gray-500 text-white">
+                  Produtos
+                </Link>
+              </li>
+              <li className="mx-4 lg:mx-0">
+                <Link to="/" className="hover:text-gray-500 text-white">
+                  Features
+                </Link>
+              </li>
+              <li className="mx-4 lg:mx-0">
+                <Link to="/" className="hover:text-gray-500 text-white">
+                  Empresas
+                </Link>
+              </li>
+              <li className="mx-4 lg:mx-0">
+                <Link to="/" className="hover:text-gray-500 text-white">
+                  Sobre
+                </Link>
+              </li>
+              <li className="mx-4 lg:mx-0">
+                <Link to="/" className="hover:text-gray-500 text-white">
+                  Contato
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-      <div className="text-white hidden lg:flex items-center justify-center">
-        <Link to="#produtos" className="mx-2 font-semibold">
-          Produtos
-        </Link>
-        <Link to="#features" className="mx-2 font-semibold">
-          Features
-        </Link>
-        <Link to="#empresas" className="mx-2 font-semibold">
-          Empresas
-        </Link>
-        <Link to="#sobre" className="mx-2 font-semibold">
-          Sobre
-        </Link>
-        <Link to="#contato" className="mx-2 font-semibold">
-          Contato
-        </Link>
-      </div>
-
-      <div className="flex lg:hidden items-center justify-center">
-        <div className="cursor-pointer hover:bg-white rounded p-2">
-          <FaBars className="text-white text-xl hover:text-black" />
+          <div className="flex lg:hidden items-center gap-6">
+            <FaBars
+              className="text-white text-xl cursor-pointer menu"
+              onClick={onToggleMenu}
+            />
+          </div>
         </div>
       </div>
     </div>
