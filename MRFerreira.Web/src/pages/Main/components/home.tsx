6a43648155bb264bd4bb3426098a51b49e4c76e8
mateus040 <mateusgabrielmoreno264@gmail.com>
2024-05-02 21:13:37 -0300

    feat: texto home (wip)

--- MRFerreira.Web/src/pages/Main/components/home.tsx
+++ MRFerreira.Web/src/pages/Main/components/home.tsx
@@ -1,31 +1,27 @@
 export default function Home() {
   return (
-    <div className="">
-      <img
-        src="/images/fundo.png"
-        className="min-w-full aspect-video object-cover fundo-home"
-      />
-
-      {/*<div className="grid grid-cols-12 gap-28 py-20 pb-32 px-8 lg:px-20 bg-black">
-      <div className="col-span-12 md:col-span-6">
-        <h3 className="uppercase text-2xl lg:text-3xl font-bold text-white">
-          O conforto que você merece!
-        </h3>
-        <p className="mt-6 text-white">
-          Descubra as nossas coleções e adquira peças que não apenas ocupam
-          espaço, mas transformam ambientes!
-        </p>
-        <button
-          className="mt-8 border-2 border-white rounded px-8 py-2 text-white hover:bg-white hover:text-black transition-all"
-          type="button"
-        >
-          Venha conhecer
-        </button>
+    <>
+      <div className="relative">
+        <img
+          src="/images/fundo.png"
+          className="min-w-full aspect-video object-cover fundo-home"
+        />
       </div>
-      <div className="col-span-12 md:col-span-6">
-        <img src="/images/log.svg" className="w-full object-contain" />
+      <div className="absolute inset-0 bg-black opacity-80 fundo-home"></div>
+      <div className="absolute inset-0 flex items-center justify-center sm:top-1/2 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:-translate-y-1/2 text-white text-center">
+        <div>
+          <h1 className="uppercase text-2xl sm:text-3xl font-bold">
+            O conforto que você merece!
+          </h1>
+          <p className="mt-5 text-md sm:text-lg text-gray-300">
+            Descubras nossas coleções e adquira peças que não ocupam espaço, mas
+            transformam ambientes!
+          </p>
+          <button className="mt-8 border-2 border-white rounded px-8 py-2 hover:bg-white hover:text-black transition-all">
+            Venha conhecer!
+          </button>
+        </div>
       </div>
-  </div>*/}
-    </div>
+    </>
   );
 }
