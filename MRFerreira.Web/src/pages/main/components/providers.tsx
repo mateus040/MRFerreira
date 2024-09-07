2ed796b85ce457676006240e96fc89be7af8d355
mateus040 <mateusgabrielmoreno264@gmail.com>
2024-09-07 11:34:57 -0300

    feat: chamada por id

--- MRFerreira.Web/src/pages/main/components/providers.tsx
+++ MRFerreira.Web/src/pages/main/components/providers.tsx
@@ -3,7 +3,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Link } from "react-router-dom";
 import FornecedorModel from "../../../interface/models/FornecedorModel";
-import formatNameForURL from "../../../utils/formatNameForURL";
 
 interface Props {
   providers: FornecedorModel[];
@@ -58,7 +57,7 @@ export const SectionProviders = ({ providers, logos }: Props) => {
                     {provider.nome}
                   </p>
                   <Link
-                    to={`/fornecedor/${formatNameForURL(provider.nome)}`}
+                    to={`/fornecedor/${provider.id}`}
                     className="flex items-center justify-center w-[230px] mt-5 -mb-5 border-2 border-black rounded px-8 py-2 hover:bg-black hover:text-white transition-all"
                   >
                     Ver catálogo
