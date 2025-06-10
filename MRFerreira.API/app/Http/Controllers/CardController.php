a8146c092c288a164f16558f57b27e2898de94c9
mateus040 <mateusgabrielmoreno264@gmail.com>
2025-06-10 08:55:50 -0300

    docs: documenting cards endpoint

--- MRFerreira.API/app/Http/Controllers/CardController.php
+++ MRFerreira.API/app/Http/Controllers/CardController.php
@@ -9,8 +9,50 @@ use App\Models\{
 };
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
+/**
+ * @OA\Tag(
+ *     name="Cards",
+ * )
+ */
 class CardController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/cards",
+     *     tags={"Cards"},
+     *     summary="Listar quantidade de produtos, fornecedores e categorias",
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(property="products_count", type="integer"),
+     *                     @OA\Property(property="providers_count", type="integer"),
+     *                     @OA\Property(property="categories_count", type="integer"),
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *        response=401,
+     *        description="Unauthorized",
+     *        content={
+     *            @OA\MediaType(
+     *                mediaType="application/json",
+     *                @OA\Schema(
+     *                    type="object",
+     *                    @OA\Property(property="message", type="string"),
+     *                )
+     *            )
+     *        }
+     *     ),
+     * )
+     */
     public function index()
     {
         $products_count = Product::count();
